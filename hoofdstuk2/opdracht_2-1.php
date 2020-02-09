c66ef1e0ee10b72080156fa2d0aa235c22e9a32e
jaelle van Anrooij <jaelle.vananrooij@edu-kw1c.nl>
2020-02-09 17:38:20 +0100

    opdracht 2.1 gemaakt

---
+++ hoofdstuk2/opdracht_2-1.php
@@ -0,0 +1,14 @@
+<!doctype HTML>
+<html lang="en">
+<head>
+    <title>
+        Opdracht X.X
+    </title>
+    <meta name="description" content="Omschrijving over de website">
+    <meta name="keywords" content="sleutelwoord1,sleutelwoord2,etc">
+    <meta charset="utf-8">
+</head>
+<body>
+<?php echo("Hello world"); ?>
+</body>
+</html>
