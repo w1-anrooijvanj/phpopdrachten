6e15e6524451f191bebdd024731d75790aabb379
jaelle van Anrooij <jaelle.vananrooij@edu-kw1c.nl>
2020-03-14 10:41:43 +0100

    Opdracht 4.1

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,24 @@
-<?php include '../hoofdstuk3/variabelen.php'; ?>
+<?php
+include '../hoofdstuk3/variabelen.php';
+?>
+
 </main>
 <footer>
-    <?php echo $name.' © '.$year;?>
+
+    <?php
+    $uur = date("H");
+    if($uur >= 0 && $uur <= 5){
+        echo ' Goedennacht bezoeker, ';
+    } elseif($uur > 5 && $uur <= 12){
+        echo ' Goedenochtend bezoeker, ';
+    } elseif($uur > 12 && $uur <= 17){
+        echo ' Goedenmiddag bezoeker, ';
+    } elseif($uur > 17 && $uur <= 24){
+        echo ' Goedenavond bezoeker, ';
+    }
+    echo $name.' © '.$year;
+    date_default_timezone_set("Europe/Amsterdam");
+    ?>
 </footer>
 </body>
 </html>
